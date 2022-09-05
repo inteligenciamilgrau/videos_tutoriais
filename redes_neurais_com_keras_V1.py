05a38c3f3fc4b815863eb2e3343265f52099f9b7
inteligenciamilgrau <48864772+inteligenciamilgrau@users.noreply.github.com>
2022-09-05 18:26:04 -0300

    Versão do vídeo
    V1

--- redes_neurais_com_keras_V1.py
+++ redes_neurais_com_keras_V1.py
@@ -1,118 +1,102 @@
 import cv2
 import numpy as np
 import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-captura = cv2.VideoCapture(2, cv2.CAP_DSHOW)
-width  = int(captura.get(cv2.CAP_PROP_FRAME_WIDTH))  # float `width`
-height = int(captura.get(cv2.CAP_PROP_FRAME_HEIGHT)) # float `height`
+cap = cv2.VideoCapture(2, cv2.CAP_DSHOW)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # float `width`
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # float `height`
 print(width, height)
 
 model = tf.keras.Sequential([
-    tf.keras.layers.Input(shape=(height,width, 3)),
+    tf.keras.layers.Input(shape=(height, width, 3)),
     tf.keras.layers.Flatten(),
-    #tf.keras.layers.Flatten(input_shape=(height,width)),
-    #tf.keras.layers.Dense(5, activation="sigmoid", name="perceptron_1", use_bias=True),
-    tf.keras.layers.Dense(10, activation="sigmoid", name="perceptron_2", use_bias=True),
-    tf.keras.layers.Dense(10, activation="sigmoid", name="perceptron_3", use_bias=True),
-    #tf.keras.layers.Dense(2, activation="softmax", name="output")
-    tf.keras.layers.Dense(1, activation="sigmoid", name="output")
-    ])
+    tf.keras.layers.Dense(100, activation="sigmoid", name="perceptron_1"),
+    tf.keras.layers.Dense(100, activation="sigmoid", name="perceptron_2"),
+    tf.keras.layers.Dense(100, activation="sigmoid", name="perceptron_3"),
+    tf.keras.layers.Dense(100, activation="sigmoid", name="perceptron_4"),
+    tf.keras.layers.Dense(100, activation="sigmoid", name="perceptron_5"),
+    #tf.keras.layers.Dense(100, activation="sigmoid", name="perceptron_6"),
+    #tf.keras.layers.Dense(100, activation="sigmoid", name="perceptron_7"),
+    #tf.keras.layers.Dense(100, activation="sigmoid", name="perceptron_8"),
+    #tf.keras.layers.Dense(100, activation="sigmoid", name="perceptron_9"),
+    #tf.keras.layers.Dense(100, activation="sigmoid", name="perceptron_10"),
+    #tf.keras.layers.Dense(1, activation="sigmoid", name="output")
+    tf.keras.layers.Dense(2, activation="softmax", name="output")
+])
 
 model.build()
 print(model.summary())
 
+model.compile(optimizer=tf.optimizers.Adam(lr=0.001),
+              loss={'output': 'mse'},
+              metrics={'output': 'accuracy'})
 
-model.compile(optimizer=tf.optimizers.Adam(lr=0.005),
-                      loss={'output': 'mse'},
-                      metrics={'output': 'accuracy'})
+initial_class = [1.0, 0.0]
+#initial_class = [1.0]
 
-#initial_class = [1, 0]#None#np.array([1.0, 0.0])
-initial_class = [1]
-
-X = [] #np.array([])
+X = []
 y = []
 images = []
 
-while (1):
-    ret, frame = captura.read()
+# construct the training image generator for data augmentation
+aug = ImageDataGenerator(rotation_range=1, zoom_range=0.0,
+                             width_shift_range=0.1, height_shift_range=0.05, shear_range=0.15,
+                             horizontal_flip=False, fill_mode="constant", validation_split=0.30)
 
-    #pre processing
-    #img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #img_gray_pre = [img_gray]
-    img_gray_np = np.array([frame])#.reshape(-1, height, width, 1)
+EPOCHS = 100
+BS = 8
 
-
-
-
-
-
-
-    # predicts
-    predict = model.predict(img_gray_np)
-    print("predict", predict)
-
+while True:
+    ret, frame = cap.read()
     cv2.imshow("Video", frame)
 
-    k = cv2.waitKey(0) & 0xff
+    k = cv2.waitKey(1) & 0xff
+
+    grab_image = False
     if k == 27 or k == ord("q"):
         break
     # select classes
     elif k == ord('1'):
-        #initial_class = [1, 0]
-        initial_class = [1]
-        #if initial_class is None:
-        #    initial_class = np.array([1.0, 0.0])
-        #else:
-        #    initial_class = np.concatenate((initial_class, [1.0, 0.0]))
+        initial_class = [1.0, 0.0]
+        #initial_class = [1.0]
+        grab_image = True
     elif k == ord('2'):
-        #initial_class = [0, 1]
-        initial_class = [0]
-        #if initial_class is None:
-        #    initial_class = np.array([0.0, 1.0])
-        #else:
-        #    initial_class = np.concatenate((initial_class, [0.0, 1.0]))
+        initial_class = [0.0, 1.0]
+        #initial_class = [0.0]
+        grab_image = True
 
     elif k == ord('t'):
-        model.fit(X, np.array(y), epochs=1, shuffle=True)
-    print("Class selected to train ########## ", initial_class)
+        model.fit(np.array(images), np.array(y), epochs=EPOCHS, shuffle=True, batch_size=BS, validation_split=0.3)
 
+    if k == ord("p"):
+        predict = model.predict(np.array([frame]))
+        predict_label = list(np.where(predict[0] >= 0.5, 1, 0))
+        max_value = max(predict_label)
+        max_index = predict_label.index(max_value)
+        labels = ["Inteligencia Mil Grau - LOGO", "DEV FEST 2018"]
+        print("Predict Label", labels[max_index])
+        print("predict", predict)
 
+    preview_gen = False
+    if preview_gen:
+        img = aug.flow(np.array([frame]), np.array([initial_class]), batch_size=BS)
+        cv2.imshow("grab", img[0][0][0].astype(np.uint8))
 
-    #train
-    #model.fit(img_gray_np, np.array(initial_class), epochs=1)
-    #print("X", X)
+    if grab_image:
+        print("Class selected to train ########## ", initial_class)
 
-    #model.fit(X, y, epochs=1)
-    #model.fit(np.array(X).reshape(-1, height, width, 1), np.array(y), epochs=1)
-    if not k == ord("n") and not k == ord("t"):
         images.append(frame)
-        print("len", len(images))
-        X = np.array(images)#.reshape(-1, height, width, 1)
-        #np.insert(X, -1, img_gray_np, axis=1)
         y.append(initial_class)
 
-        #train_dataset = tf.data.Dataset.from_tensor_slices((X, y))
+        for i in range(0):
+            img = aug.flow(np.array([frame]), np.array([initial_class]), batch_size=BS)
 
-        ##BATCH_SIZE = 64
-        #SHUFFLE_BUFFER_SIZE = 100
+            images.append(img[0][0][0].astype(np.uint8))
+            y.append(initial_class)
 
-        #train_dataset = train_dataset.shuffle(SHUFFLE_BUFFER_SIZE)#.batch(BATCH_SIZE)
-
-        #for i in range(1):
-         #   for j in range(1):#len(X)):
-           #     print("i", j, y[j])
-                #model.fit(np.array(X[i]).reshape(-1, height, width, 1), np.array(y[i]), epochs=1)
-                #model.fit(X[j], np.array(y[j]), epochs=1)
-                #model.fit([img_gray_np], [[1]], epochs=1)
-        #model.fit(train_dataset, epochs=1000)
-        model.fit(X,np.array(y), epochs=1)
         print(initial_class)
-        #model.fit(X, [initial_class], epochs=100)
-        print("Treinei")
-    print("y", y)
+        print("y", y)
 
-
-
-captura.release()
+cap.release()
 cv2.destroyAllWindows()
-
