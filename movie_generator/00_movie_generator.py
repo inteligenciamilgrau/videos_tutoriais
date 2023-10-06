df842245c80a1743934ff14b9b5f3391f17d8866
Inteligência Mil Grau <48864772+inteligenciamilgrau@users.noreply.github.com>
2023-10-05 22:26:14 -0300

    Update 00_movie_generator.py

--- movie_generator/00_movie_generator.py
+++ movie_generator/00_movie_generator.py
@@ -1,8 +1,8 @@
 print("Fazendo uma automação para criar vídeos e 7 etapas")
 
-### 1) PyTube - Download do Audio
+### 1) PyTube - Download do Audio do Video
 ### 2) Whisper - Transcrição do Audio
-### 3) ChatGPT - Tradução, Melhorias, Resumo e Título
+### 3) ChatGPT - Tradução, Melhorias, Resumo, Descrição da Imagem e Título
 ### 4) Dall-E - Geração da Imagem
 ### 5) Requests - Baixar imagem gerada
 ### 6) PyTTSx3 - Criar som (pode ser feito tbm com a API da ElevenLabs)
